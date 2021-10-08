8f5753fb8fd28422cc042075efad31d91b26db0d
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-08 20:52:02 +0200

    Created function to count neighbours and rules of the game

---
+++ GameOfLife.js
@@ -0,0 +1,89 @@
+const cuadricula = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
+console.log(cuadricula);
+
+function inicioDelJuego(fila, columna) {
+  //Empezar con todo zero
+  for (let i = 0; i < filas; i++) {
+    const cuadricula = [];
+    for (let j = 0; j < columnas; j++) {
+      cuadricula[i][j] = 0;
+    }
+  }
+  return cuadricula;
+}
+
+function comprobarVecinos(fila, columna) {
+  let totalVecinos = 0;
+
+  if (fila >= 0 && columna >= 0) {
+    totalVecinos += cuadricula[fila - 1][columna - 1];
+  }
+
+  if (fila >= 0) {
+    totalVecinos += cuadricula[fila - 1][columna];
+  }
+
+  if (fila >= 0 && columna < 5) {
+    totalVecinos += cuadricula[fila - 1][columna + 1];
+  }
+
+  if (fila < 5 && columna >= 0) {
+    totalVecinos += cuadricula[fila + 1][columna - 1];
+  }
+
+  if (columna < 5) {
+    totalVecinos += cuadricula[fila][columna + 1];
+  }
+
+  if (fila < 5 && columna >= 0) {
+    totalVecinos += cuadricula[fila + 1][columna - 1];
+  }
+
+  if (fila < 5) {
+    totalVecinos += cuadricula[fila + 1][columna];
+  }
+
+  if (fila < 5 && columna < 5) {
+    totalVecinos += cuadricula[fila + 1][columna + 1];
+  }
+
+  return totalVecinos;
+}
+
+console.log(comprobarVecinos(0, 2));
+
+function vivirMorir(fila, columna) {
+  for (let i = 0; i < fila; i++) {
+    for (let j = 0; j < columna; j++) {
+      let marcador = comprobarVecinos(i, j);
+      if (cuadricula[i][j] === 1) {
+        if (totalVecinos === 2 || totalVecinos === 3) {
+        } else {
+          cuadricula[i][j] = 0;
+        }
+      } else if (cuadricula[i][j] === 0) {
+        if (totalVecinos === 3) {
+          cuadricula[i][j] = 1;
+        }
+      }
+    }
+  }
+}
+
+//Poblar cuadricula a boleo
+
+function poblar(fila, columna) {
+  for (let i = 0; i < fila; i++) {
+    for (let j = 0; j < columna; j++) {
+      cuadricula[i][j] = Math.round(Math.random() * 1 + 1);
+    }
+  }
+  return cuadricula;
+}

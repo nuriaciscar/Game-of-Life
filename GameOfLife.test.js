b9b49aa20ae709b7a1007b20e8ca2278b278f04a
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-09 19:42:50 +0200

    Added index.html and styles.css

--- GameOfLife.test.js
+++ GameOfLife.test.js
@@ -33,8 +33,8 @@ describe("Given a checkNeighbours function", () => {
       expect(result).toEqual(expected);
     });
   });
-  describe("When it receives checkCompany(0,0)", () => {
-    test("Then it should return 2 neighbours", () => {
+  describe("When it receives [0, 1, 0],[0, 1, 0],[0, 1, 0],", () => {
+    test("Then it should return [0, 0, 0],[1, 1, 1],[0, 0, 0],", () => {
       const input = [
         [0, 1, 0],
         [0, 1, 0],
@@ -47,6 +47,34 @@ describe("Given a checkNeighbours function", () => {
         [0, 0, 0],
       ];
 
+      const result = matrix.checkMatrix(input);
+
+      expect(result).toEqual(expected);
+    });
+  });
+  describe("When it receives an specific array", () => {
+    test("Then it should return an array", () => {
+      const input = [
+        [1, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 1],
+      ];
+
+      const expected = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+      ];
       const result = matrix.checkMatrix(input);
 
       expect(result).toEqual(expected);

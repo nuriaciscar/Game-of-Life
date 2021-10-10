419cfa865a508cab1fa0b750d52c8aa6a1c1f0d6
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-10 17:41:45 +0200

    Created function cleanGame

--- GameOfLife.js
+++ GameOfLife.js
@@ -1,29 +1,54 @@
 const boardHTML = document.getElementsByClassName("game__grid__board");
 const columns = document.getElementsByClassName("game__grid__board__column");
 
+let matrix = [];
+
 function createBoard() {
+  const newBoardGrid = [];
   const newBoard = document.createElement("div");
   newBoard.className = "game__grid__board__new-board";
-  for (let i = 0; i < 87; i++) {
+  // for (let i = 0; i < 85; i++) {
+  //   newBoard[i] = [];
+  //   const column = document.createElement("div");
+  //   column.className = "game__grid__board__column";
+  //   boardHTML[0].appendChild(newBoard);
+  //   newBoard.appendChild(column);
+  // }
+  for (let i = 0; i < 85; i++) {
+    newBoardGrid[i] = [];
     const column = document.createElement("div");
     column.className = "game__grid__board__column";
     boardHTML[0].appendChild(newBoard);
     newBoard.appendChild(column);
-  }
-  for (let i = 0; i < 87; i++) {
     for (let j = 0; j < 40; j++) {
+      newBoardGrid[i][j] = 0;
       const cell = document.createElement("div");
       cell.className = "game__grid__board__cell";
       cell.id = `${i}-${j}`;
+
       cell.style.backgroundColor = "transparent";
+      cell.onclick = changeColor;
       columns[i].appendChild(cell);
     }
   }
+  matrix = newBoardGrid;
 }
 
 createBoard();
 
-let matrix = [];
+function changeColor() {
+  const position = this.id.split("-");
+  const horitzontal = position[0];
+  const vertical = position[1];
+
+  if (this.style.backgroundColor === "transparent") {
+    this.style.backgroundColor = "black";
+    matrix[horitzontal][vertical] = 1;
+  } else {
+    this.style.backgroundColor = "transparent";
+    matrix[horitzontal][vertical] = 0;
+  }
+}
 
 function newBoard(rows, columns) {
   for (let i = 0; i < rows; i++) {
@@ -34,14 +59,6 @@ function newBoard(rows, columns) {
   }
   return matrix;
 }
-
-matrix = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0],
-];
 
 function checkCompany(matrix, i, j) {
   let count = 0;
@@ -110,3 +127,12 @@ module.exports = {
   createBoard,
   newBoard,
 };
+
+function cleanGame() {
+  for (let i = 0; i < 85; i++) {
+    for (let j = 0; j < 40; j++) {
+      newBoardGrid[i][j] = 0;
+      cell.style.backgroundColor = "transparent";
+    }
+  }
+}
